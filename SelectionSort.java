1342240002063806479c05ed7f66697f3774e631
Vaishnavi Katare <vaishnavikatare337@gmail.com>
2026-09-06 15:45:45 +0530

    Added file of Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args){
+        int arr[] = {4,5,-18,22,43,89,1024};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void selection(int[] arr){
+        for(int i =0; i <arr.length; i++){
+            int last = arr.length-i-1;
+            int max = getMax(arr,0,last);
+            swap(arr,max,last);
+        }
+    }
+    static int getMax(int[] arr, int start ,int end){
+        int max = start;
+        for(int i = start ; i<=end;i++){
+            if(arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+
+    }
+
+    static void swap(int[] arr, int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second]= temp;
+    }
+    
+}
